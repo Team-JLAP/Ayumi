85ce773237809c476a3cdc0cfcded6e691394da4
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-12-06 13:43:36 -0800

    created additional routers

--- professors/views.py
+++ professors/views.py
@@ -17,11 +17,22 @@ def search_prof(request, prof_name):
 def search_course(request, subject, course_id):
     return render(request, 'professors/search_course.html')
 
-def prof_course(request, prof_name):
+def prof_course(request, prof_id):
     return render(request, 'professors/prof_course.html')
+
+def rating(request, prof_id, course_name):
+    return render(request, 'professors/rating.html', context={'prof_id': prof_id})
+
+
 
 def login(request):
     return render(request, 'professors/login.html')
 
-def rating(request, prof_id, course_name):
-    return render(request, 'professors/rating.html', context={'prof_id': prof_id})
+def signup(request):
+    return render(request, 'professors/signup.html')
+
+def profile(request, user_id):
+    return render(request, 'professors/profile.html')
+
+def profile_setting(request, user_id):
+    return render(request, 'professors/profile_setting.html')
