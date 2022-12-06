9debcbb95eec49c1654bb3778027dad5999bc74f
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-12-05 22:01:30 -0800

    set urls and their names

--- professors/views.py
+++ professors/views.py
@@ -4,18 +4,24 @@ from .dummy import generateProfList
 def home(request):
     return render(request, 'professors/home.html')
 
-def prof(request, prof_id):
+def course_ratings(request, prof_id, course_name):
     target = generateProfList(1)[0]
     context = {'prof': target}
-    return render(request, 'professors/prof.html', context=context)
+    return render(request, 'professors/course_ratings.html', context=context)
 
-def search_result(request, prof_name):
+def search_prof(request, prof_name):
     profs = generateProfList(5)
     context = {'search_term': prof_name, 'results': profs}
-    return render(request, 'professors/search_result.html', context=context)
+    return render(request, 'professors/search_prof.html', context=context)
+
+def search_course(request, subject, course_id):
+    return render(request, 'professors/search_course.html')
+
+def prof_course(request, prof_name):
+    return render(request, 'professors/prof_course.html')
 
 def login(request):
     return render(request, 'professors/login.html')
 
-def rating(request, prof_id):
+def rating(request, prof_id, course_name):
     return render(request, 'professors/rating.html', context={'prof_id': prof_id})
