310c0865d41ca8406fa119e3207b9a1d902c7de4
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-12-06 14:32:27 -0800

    connect mysql database

--- ayumi/settings.py
+++ ayumi/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'ayumi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'ayumi_db',
+        'USER': 'root',  
+        'PASSWORD': 'H2takenoko8989',  
     }
 }
 
