b2ec41fbc9eb37ee6f28cacdfd199d1ac9cb8adf
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-12-05 22:52:22 -0800

    changed prof name to id

--- professors/urls.py
+++ professors/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('prof=<int:prof_id>/<str:course_name>/', views.course_ratings, name='course_ratings'),
+    path('profid=<int:prof_id>/<str:course_name>/', views.course_ratings, name='course_ratings'),
     path('search/prof=<str:prof_name>/', views.search_prof, name='search_prof'),
     path('search/subject=<str:subject>/id=<int:course_id>/', views.search_course, name='search_course'),
-    path('course/prof=<str:prof_name>/', views.prof_course, name='prof_course'),
+    path('course/profid=<int:prof_id>/', views.prof_course, name='prof_course'),
     path('login/', views.login, name='login'),
     path('rating/prof=<int:prof_id>/<str:course_name>/', views.rating, name='rating')
     # user profile, profile setting, etc.
